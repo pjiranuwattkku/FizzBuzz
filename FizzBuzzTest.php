72ccd5f1e951796043e59513f3eb85b103580877
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 09:56:25 +0700

    add test case testFizzBuzzFourShouldBeFour

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,12 +1,16 @@
 <?php
 class FizzBuzz{
   function count($number){
-    $_FIZZBUZZ_TEXT = [1=>"1",2=>"2",3=>"Fizz"];
+    $_FIZZBUZZ_TEXT = [1=>"1",2=>"2",3=>"Fizz",4=>"4"];
     return $_FIZZBUZZ_TEXT[$number];
   }
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+  function testFizzBuzzFourShouldBeFour(){
+    $fizzbuzz = new FizzBuzz();
+    $this->assertEquals("4",$fizzbuzz->count(4));
+  }
   function testFizzBuzzThreeShouldBeFizz(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("Fizz",$fizzbuzz->count(3));
