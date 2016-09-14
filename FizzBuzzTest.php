66c5c057d61b2a8ebdc5428015a641e65ce529e0
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 10:32:48 +0700

    Refactor code change complex logic splite to function

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,16 +1,29 @@
 <?php
 class FizzBuzz{
   function count($number){
-    if($number%3 == 0 && $number%5 == 0){
+    if($this->isModuloBythreeAndFive($number)){
       return "FizzBuzz";
-    }else if($number%3 == 0){
+    }else if($this->isModuloByThree($number)){
       return "Fizz";
-    }else if($number%5 == 0){
+    }else if($this->isModuloByFive($number)){
       return "Buzz";
     }else{
         return $number;
     }
   }
+
+  public function isModuloByThreeAndFive($number){
+    return $number%3 == 0 && $number%5 == 0;
+  }
+
+  public function isModuloByThree($number){
+    return $number%3 == 0;
+  }
+
+  public function isModuloByFive($number){
+    return $number%5 == 0;
+  }
+
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
