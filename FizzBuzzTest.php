7ef27d1f1cd80cbd054a5bfecc5a972f5783f331
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 09:45:07 +0700

    Add Test Case testFizzBuzzShouldBeTwo

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -7,10 +7,15 @@ class FizzBuzz{
     if($this->number == 1){
       return "1";
     }
+    return "2";
   }
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+  function testFizzBuzzShouldBeTwo(){
+    $fizzbuzz = new FizzBuzz(2);
+    $this->assertEquals('2',$fizzbuzz->count());
+  }
   function testFizzBuzzShouldBeOne(){
     $fizzbuzz = new FizzBuzz(1);
     $this->assertEquals('1',$fizzbuzz->count());
