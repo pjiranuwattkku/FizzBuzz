f242972fe83ebe6d14212b6f058dca22780c516f
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 10:42:19 +0700

    refactor test case

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -27,79 +27,26 @@ class FizzBuzz{
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-  function testFizzBuzzFifteenShouldBeFifteen(){
+
+  function testIsmoduloByThreeAndFiveShouldBeFizzBuzz(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("FizzBuzz",$fizzbuzz->count(15));
   }
 
-  function testFizzBuzzFourteenShouldBeFourteen(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("14",$fizzbuzz->count(14));
-  }
-
-  function testFizzBuzzThirteenShouldBeThirteen(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("13",$fizzbuzz->count(13));
-  }
-
-  function testFizzBuzzTwelveShouldBeFizz(){
+  function testIsmoduloByThreeShouldBeFizz(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("Fizz",$fizzbuzz->count(12));
   }
 
-  function testFizzBuzzElevenShouldBeEleven(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("11",$fizzbuzz->count(11));
-  }
-
-  function testFizzBuzzTenShouldBeBuzz(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("Buzz",$fizzbuzz->count(10));
-  }
-
-  function testFizzBuzzNineShouldBeFizz(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("Fizz",$fizzbuzz->count(9));
-  }
-
-  function testFizzBuzzEightShouldBeEight(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("8",$fizzbuzz->count(8));
-  }
-
-  function testFizzBuzzSevenShouldBeSeven(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("7",$fizzbuzz->count(7));
-  }
-
-  function testFizzBuzzSixShouldBeFizz(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("Fizz",$fizzbuzz->count(6));
-  }
-
-  function testFizzBuzzFiveShouldBeBuzz(){
+  function testIsmoduloByFiveShouldBeBuzz(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("Buzz",$fizzbuzz->count(5));
   }
 
-  function testFizzBuzzFourShouldBeFour(){
+  function testIsOtherNumber(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("4",$fizzbuzz->count(4));
   }
 
-  function testFizzBuzzThreeShouldBeFizz(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("Fizz",$fizzbuzz->count(3));
-  }
-
-  function testFizzBuzzOneShouldBeTwo(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("2",$fizzbuzz->count(2));
-  }
-
-  function testFizzBuzzOneShouldBeOne(){
-    $fizzbuzz = new FizzBuzz();
-    $this->assertEquals("1",$fizzbuzz->count(1));
-  }
 }
 ?>
