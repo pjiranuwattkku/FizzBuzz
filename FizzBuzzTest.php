85db4932ac94aa549c721ec2b84e0b4e2ae881b0
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 10:10:54 +0700

    add test case fifteen and fouteen

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,12 +1,22 @@
 <?php
 class FizzBuzz{
   function count($number){
-    $_FIZZBUZZ_TEXT = [1=>"1",2=>"2",3=>"Fizz",4=>"4",5=>"Buzz",6=>"Fizz",7=>"7",8=>"8",9=>"Fizz",10=>"Buzz",11=>"11",12=>"Fizz",13=>"13"];
+    $_FIZZBUZZ_TEXT = [1=>"1",2=>"2",3=>"Fizz",4=>"4",5=>"Buzz",6=>"Fizz",7=>"7",8=>"8",9=>"Fizz",10=>"Buzz",11=>"11",12=>"Fizz",13=>"13",14=>"14",15=>"FizzBuzz"];
     return $_FIZZBUZZ_TEXT[$number];
   }
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+  function testFizzBuzzFifteenShouldBeFifteen(){
+    $fizzbuzz = new FizzBuzz();
+    $this->assertEquals("FizzBuzz",$fizzbuzz->count(15));
+  }
+
+  function testFizzBuzzFourteenShouldBeFourteen(){
+    $fizzbuzz = new FizzBuzz();
+    $this->assertEquals("14",$fizzbuzz->count(14));
+  }
+
   function testFizzBuzzThirteenShouldBeThirteen(){
     $fizzbuzz = new FizzBuzz();
     $this->assertEquals("13",$fizzbuzz->count(13));
