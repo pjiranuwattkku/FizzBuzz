74917ede9b6ffdd6e7cb3ffebbbf4a454c0e1e07
pjiranuwatt <pjiranuwatt@kku.ac.th>
2016-09-14 09:54:52 +0700

    refactor code

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,21 +1,23 @@
 <?php
 class FizzBuzz{
   function count($number){
-    if($number == 1){
-      return "1";
-    }
-    return "2";
+    $_FIZZBUZZ_TEXT = [1=>"1",2=>"2",3=>"Fizz"];
+    return $_FIZZBUZZ_TEXT[$number];
   }
 }
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-  function testFizzBuzzShouldBeTwo(){
+  function testFizzBuzzThreeShouldBeFizz(){
     $fizzbuzz = new FizzBuzz();
-    $this->assertEquals('2',$fizzbuzz->count(2));
+    $this->assertEquals("Fizz",$fizzbuzz->count(3));
   }
-  function testFizzBuzzShouldBeOne(){
+  function testFizzBuzzOneShouldBeTwo(){
     $fizzbuzz = new FizzBuzz();
-    $this->assertEquals('1',$fizzbuzz->count(1));
+    $this->assertEquals("2",$fizzbuzz->count(2));
+  }
+  function testFizzBuzzOneShouldBeOne(){
+    $fizzbuzz = new FizzBuzz();
+    $this->assertEquals("1",$fizzbuzz->count(1));
   }
 }
 ?>
